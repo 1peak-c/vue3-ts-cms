7aab53d6cee6a15f260f0d5fb6d331f8ed3614a6
成一峰 <947564314@qq.com>
2022-03-08 17:07:03 +0800

    vue环境变量

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// import { BASE_URL, BASE_NAME } from './service/request/config'
+// console.log(BASE_URL)
+// console.log(BASE_NAME)
+// console.log(123)
 // 全局引入
 // import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
---
+++ src/service/request/config.ts
@@ -0,0 +1,17 @@
+// const BASE_URL = 'http://coderwhy.org/prod'
+// const BASE_NAME = 'cyf'
+
+// 根据process.env.NODE_ENV 判断环境  不需要手动修改
+
+export let BASE_URL = ''
+export let BASE_NAME = ''
+if (process.env.NODE_ENV === 'production') {
+  BASE_URL = 'http://coderwhy.org/prod'
+  BASE_NAME = 'cyfP'
+} else if (process.env.NODE_ENV === 'development') {
+  BASE_URL = 'http://coderwhy.org/deve'
+  BASE_NAME = 'cyfD'
+} else if (process.env.NODE_ENV === 'text') {
+  BASE_URL = 'http://coderwhy.org/text'
+  BASE_NAME = 'cyfT'
+}
